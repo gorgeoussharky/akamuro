a1560844e7cc5c2f9e1cb7d7ba51a30f0354ab48
gorgeoussharky <gorgeoussharky@gmail.com>
2021-01-08 03:19:37 +0300

    fixes

--- src/components/Advantages.vue
+++ src/components/Advantages.vue
@@ -1,80 +1,94 @@
 <template>
-  <div class="advantages__wrap">
-    <div class="advantages__bg">
-      <svg
-        width="1110"
-        height="425"
-        viewBox="0 0 1110 423"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <mask
-          id="mask0"
-          mask-type="alpha"
-          maskUnits="userSpaceOnUse"
-          x="0"
-          y="0"
+  <div class="container">
+    <div class="advantages__wrap">
+      <div class="advantages__bg">
+        <svg
           width="1110"
-          height="423"
+          height="425"
+          viewBox="0 0 1110 423"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <path d="M0 1H354.713L552 122" stroke="white" stroke-width="2" />
-          <path d="M0 141H354.556L551.5 122" stroke="white" stroke-width="2" />
-          <path d="M0 281H354.314L551.5 123" stroke="white" stroke-width="2" />
-          <path d="M0 422H354.314L552 122.5" stroke="white" stroke-width="2" />
-          <path
-            d="M1109.5 1H754.787L551.5 121.5"
-            stroke="white"
-            stroke-width="2"
-          />
-          <path
-            d="M1109.5 141H754.944L552 123"
-            stroke="white"
-            stroke-width="2"
-          />
-          <path
-            d="M1109.5 281H755.186L552 122.5"
-            stroke="white"
-            stroke-width="2"
-          />
-          <path
-            d="M1109.5 422H755.186L551.5 121.5"
-            stroke="white"
-            stroke-width="2"
-          />
-        </mask>
-        <g mask="url(#mask0)">
-          <rect
-            x="-1"
-            y="-21"
+          <mask
+            id="mask0"
+            mask-type="alpha"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
             width="1110"
-            height="508"
-            fill="url(#paint0_radial)"
-          />
-        </g>
-        <defs>
-          <radialGradient
-            id="paint0_radial"
-            cx="0"
-            cy="0"
-            r="1"
-            gradientUnits="userSpaceOnUse"
-            gradientTransform="translate(554 135) rotate(90) scale(352 769.134)"
+            height="423"
           >
-            <stop stop-color="white" stop-opacity="0" />
-            <stop offset="1" stop-color="#C4C4C4" />
-          </radialGradient>
-        </defs>
-      </svg>
-    </div>
+            <path d="M0 1H354.713L552 122" stroke="white" stroke-width="2" />
+            <path
+              d="M0 141H354.556L551.5 122"
+              stroke="white"
+              stroke-width="2"
+            />
+            <path
+              d="M0 281H354.314L551.5 123"
+              stroke="white"
+              stroke-width="2"
+            />
+            <path
+              d="M0 422H354.314L552 122.5"
+              stroke="white"
+              stroke-width="2"
+            />
+            <path
+              d="M1109.5 1H754.787L551.5 121.5"
+              stroke="white"
+              stroke-width="2"
+            />
+            <path
+              d="M1109.5 141H754.944L552 123"
+              stroke="white"
+              stroke-width="2"
+            />
+            <path
+              d="M1109.5 281H755.186L552 122.5"
+              stroke="white"
+              stroke-width="2"
+            />
+            <path
+              d="M1109.5 422H755.186L551.5 121.5"
+              stroke="white"
+              stroke-width="2"
+            />
+          </mask>
+          <g mask="url(#mask0)">
+            <rect
+              x="-1"
+              y="-21"
+              width="1110"
+              height="508"
+              fill="url(#paint0_radial)"
+            />
+          </g>
+          <defs>
+            <radialGradient
+              id="paint0_radial"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(554 135) rotate(90) scale(352 769.134)"
+            >
+              <stop stop-color="white" stop-opacity="0" />
+              <stop offset="1" stop-color="#C4C4C4" />
+            </radialGradient>
+          </defs>
+        </svg>
+      </div>
 
-    <ul class="advantages__list">
-      <li class="advantages__item" :key="key" v-for="(listItem, key) in list">
-        <h2 class="advantages__item-title">{{ listItem.title }}</h2>
-        <p class="advantages__item-description">
-          {{ listItem.description }}
-        </p>
-      </li>
-    </ul>
+      <ul class="advantages__list">
+        <li class="advantages__item" :key="key" v-for="(listItem, key) in list">
+          <h2 class="advantages__item-title">{{ listItem.title }}</h2>
+          <p class="advantages__item-description">
+            {{ listItem.description }}
+          </p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
